882dd51ed1b4acc28283084b8a117e2c4876398d
Eric Quackenbush <eric.quackenbush@avoxi.com>
2021-11-24 16:36:48 -0500

    quick commit before refactor

--- lists/tests.py
+++ lists/tests.py
@@ -10,3 +10,10 @@ class HomePageTest(TestCase):
         """Check the html."""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_can_save_a_post_request(self):
+        """Check for successful POST."""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
