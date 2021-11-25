5452dc93de943304ed3dfd23d69d81250d0c8cf4
Eric Quackenbush <eric.quackenbush@avoxi.com>
2021-11-25 10:52:07 -0500

    3 changed files plus 2 migrations

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,12 @@
 """docstring"""
 from django.db import models
 
+class List(models.Model):
+    """docstring"""
+    pass
+
 
 class Item(models.Model):
     """docstring"""
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
