4f3d2ca9a174e1616df7e9a4e92c117d04d3f221
Eric Quackenbush <eric.quackenbush@avoxi.com>
2021-11-25 18:14:06 -0500

    first working CSS

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,13 @@
 """docstring"""
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """docstring"""
 
     def setUp(self):
