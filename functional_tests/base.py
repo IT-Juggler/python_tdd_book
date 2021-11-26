e5ec88180f8ac8adf18beabe4e864af1da48f0ec
Eric Quackenbush <eric.quackenbush@avoxi.com>
2021-11-25 21:17:41 -0500

    moved FTs into separate files

---
+++ functional_tests/base.py
@@ -0,0 +1,37 @@
+"""docstring"""
+import os
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    """docstring"""
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        """Helper method
+        Used by test_can_start_a_list_and_retrieve_it_later.
+        """
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as wait_error:
+                if time.time() - start_time > MAX_WAIT:
+                    raise wait_error
+                time.sleep(0.5)
---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,24 @@
+"""docstring"""
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+    """docstring"""
+
+    def test_cannot_add_empty_list_items(self):
+        """docstring"""
+        # Edit goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box.
+
+        # The home page refreshes, and there is an error message stating
+        # that the list item cannot be blank.
+
+        # She tries again with some text for the item, which now works.
+
+        # Perversely, she now decides to submit a second blank list item.
+
+        # She receives a similar warning on the list page.
+
+        # And she can correct it by filling some text in.
+        self.fail('write me!')
